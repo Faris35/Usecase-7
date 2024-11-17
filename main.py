4495e31cc5b1a1c70141412abcde49b534c58302
Faris Alhamdan <124065304+Faris35@users.noreply.github.com>
2024-11-18 00:11:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -25,22 +25,17 @@ def preprocessing(input_features: InputFeatures):
         'award': input_features.award,
         'highest_value': input_features.highest_value
         }
-    # Convert dictionary values to a list in the correct order
     features_list = [dict_f[key] for key in sorted(dict_f)]
-    # Scale the input features
     scaled_features = scaler.transform([list(dict_f.values())])
 
     return scaled_features
 
-
 app = FastAPI()
 
-# GET request
 @app.get("/")
 def read_root():
     return {"message": "Welcome to Tuwaiq Academy"}
 
-# get request
 @app.get("/items/")
 def create_item(item: dict):
     return {"item": item}
@@ -49,7 +44,6 @@ def create_item(item: dict):
 async def predict(input_features: InputFeatures):
     try:
         data = preprocessing(input_features)
-        # Find the closest cluster core sample for the new data
         core_samples = model.components_
         cluster_labels, _ = pairwise_distances_argmin_min(data, core_samples)
         cluster_label = model.labels_[model.core_sample_indices_[cluster_labels[0]]]
